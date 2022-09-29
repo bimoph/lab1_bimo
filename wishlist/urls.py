df827543a339f2e8f63b7b8e0109d66875cca40c
Bimo Priyohutomo <bimopriyohutomo@Bimos-MacBook-Air.local>
2022-09-29 14:00:24 +0700

    lab4

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
-from wishlist.views import show_wishlist, show_wishlistjson, show_filter
+from wishlist.views import show_wishlist, show_wishlistjson, show_filter, register, login_user, logout_user
 
 
 app_name = 'wishlist'
 
 urlpatterns = [
-    # path('', show_wishlist, name='show_wishlist'),
+    path('', show_wishlist, name='show_wishlist'),
     path('xml/', show_wishlist, name='show_wishlist'),
     path('json/', show_wishlistjson, name='show_wishlistjson'),
-    path('json/<int:id>', show_filter, name='show_filter')
-
+    path('json/<int:id>', show_filter, name='show_filter'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 
 ]
